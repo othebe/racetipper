fdcb2198cbbb3f0443a446034cd6158daee66728
unknown <Ozzy@Ozzy-PC.(none)>
2013-01-09 16:28:04 +0800

    default images for stages/races/competitions

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,3 +4,10 @@ STATUS = {
 	:INACTIVE => 2,
 	:PRIVATE => 3
 }
+
+DEFAULT = {
+	:RACE_IMG => '/assets/default_competition.jpg',
+	:COMPETITION_IMG => '/assets/default_competition.jpg',
+	:STAGE_IMG => '/assets/default_competition.jpg',
+	:USER_IMG => '/assets/default_competition.jpg'
+}
