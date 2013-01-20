e5683bb9154cd8b7ba2d0b2bf062b3608c1b8072
unknown <Ozzy@Ozzy-PC.(none)>
2013-01-20 16:34:38 +0800

    skip email for null

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -2,7 +2,7 @@ class AppMailer < ActionMailer::Base
   default from: "from@example.com"
   
 	def competition_invitation(emails, competition)
-		return if (emails.nil || emails.strip.length==0)
+		return if (emails.nil? || emails.strip.length==0)
 		creator = User.find_by_id(competition.creator_id)
 		@competition = competition
 		@invitor_name = (creator.firstname.strip.capitalize+' '+creator.lastname.strip.capitalize).strip
