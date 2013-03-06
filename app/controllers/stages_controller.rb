73ae729fcefb36ef44b721b4b72c07c9d85789dc
unknown <Ozzy@Ozzy-PC.(none)>
2013-03-06 11:15:26 +0800

    Stage ordering on stage details page

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -3,7 +3,7 @@ class StagesController < ApplicationController
 		stage_id = params[:id]
 		@stage = Stage.where({:id=>stage_id}).joins(:race).first
 		@results = Result.get_results('stage', stage_id)
-		@stages = Stage.where({:race_id=>@stage.race_id})
+		@stages = Stage.where({:race_id=>@stage.race_id}).order('starts_on')
 		
 		render :layout=>false
 	end
