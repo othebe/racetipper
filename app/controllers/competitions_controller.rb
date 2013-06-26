e34ad54285ec18f43f368350216b104c98403108
O. Thebe <othebe@gmail.com>
2013-06-27 01:22:50 +0800

    Still need to use competition stages (phase out later)

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -566,19 +566,19 @@ class CompetitionsController < ApplicationController
 		end
 		
 		#Save races/stages
-		#CompetitionStage.update_all({:status=>STATUS[:DELETED]}, {:competition_id=>competition.id})
-		#race_id = competition_data[:race_id]
-		#competition_races = []
-		#stages = Stage.where('race_id=? AND season_id=? AND status=?', race_id, season_id, STATUS[:ACTIVE])
-		#stages.each do |stage|
-		#	competition_stage = CompetitionStage.where('stage_id=? AND competition_id=? AND status=?', stage.id, competition.id, STATUS[:ACTIVE]).first
-		#	competition_stage ||= CompetitionStage.new
-		#	competition_stage.competition_id = competition.id
-		#	competition_stage.stage_id = stage.id
-		#	competition_stage.race_id = race_id
-		#	competition_stage.status = STATUS[:ACTIVE]
-		#	competition_stage.save
-		#end
+		CompetitionStage.update_all({:status=>STATUS[:DELETED]}, {:competition_id=>competition.id})
+		race_id = competition_data[:race_id]
+		competition_races = []
+		stages = Stage.where('race_id=? AND season_id=? AND status=?', race_id, season_id, STATUS[:ACTIVE])
+		stages.each do |stage|
+			competition_stage = CompetitionStage.where('stage_id=? AND competition_id=? AND status=?', stage.id, competition.id, STATUS[:ACTIVE]).first
+			competition_stage ||= CompetitionStage.new
+			competition_stage.competition_id = competition.id
+			competition_stage.stage_id = stage.id
+			competition_stage.race_id = race_id
+			competition_stage.status = STATUS[:ACTIVE]
+			competition_stage.save
+		end
 		
 		#Set default tips (also needed if new stages were added)
 		participants = CompetitionParticipant.where({:competition_id=>competition.id, :status=>STATUS[:ACTIVE]})
