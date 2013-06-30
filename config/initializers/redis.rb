814299fc6df54ae623302d6ed924198d37c21c88
O. Thebe <othebe@gmail.com>
2013-07-01 05:02:08 +0800

    Removing Resque autostarter

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,3 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Resque.redis = REDIS
-
-#Start leaderboard generation loop
-ResqueTasks::cron_leaderboard
